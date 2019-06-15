f7cf6228a21d896d59030a61f6e884dda9e2f1cd
Ricardo Lelis <ricardo.lelis3@gmail.com>
2019-06-15 20:01:29 +0100

    Added missing Statistics section on user profile page and respective tests

--- project/tests/test_users.py
+++ project/tests/test_users.py
@@ -120,7 +120,7 @@ class UserTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Email Address', response.data)
         self.assertIn(b'Account Actions', response.data)
-        # self.assertIn(b'Statistics', response.data)
+        self.assertIn(b'Statistics', response.data)
         self.assertIn(b'First time logged in. Welcome!', response.data)
  
     def test_user_profile_page_after_logging_in(self):
@@ -132,7 +132,7 @@ class UserTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Email Address', response.data)
         self.assertIn(b'Account Actions', response.data)
-        # self.assertIn(b'Statistics', response.data)
+        self.assertIn(b'Statistics', response.data)
         self.assertIn(b'Last Logged In: ', response.data)
     
     def test_user_profile_without_logging_in(self):
