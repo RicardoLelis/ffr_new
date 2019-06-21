0fe2f2e378bf4d4aad74e992bccd1455748a4134
Ricardo Lelis <ricardo.lelis3@gmail.com>
2019-06-21 23:36:22 +0100

    Added custom error pages 404, 403, and 410 error codes

--- project/users/views.py
+++ project/users/views.py
@@ -258,4 +258,20 @@ def admin_view_users():
         return render_template('admin_view_users.html', users=users)
     return redirect(url_for('index'))
 
+############################
+#### custom error pages ####
+############################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),  404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'),  403
+
+@app.errorhandler(410)
+def page_not_found(e):
+    return render_template('410.html'),  410
+
 
